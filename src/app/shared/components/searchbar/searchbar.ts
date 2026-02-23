94e8138840296fe1eb80d383f7fdb6292816a96d
ElAguilaPrograma <je.munozm@outlook.com>
2026-02-22 21:09:31 -0600

    Commit before prototypes

--- src/app/shared/components/searchbar/searchbar.ts
+++ src/app/shared/components/searchbar/searchbar.ts
@@ -2,11 +2,10 @@ import { Component, Input, output, model } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { heroMagnifyingGlass, heroXMark } from '@ng-icons/heroicons/outline';
 import { FormsModule } from '@angular/forms';
-import { IconButton } from "../icon-button/icon-button";
 
 @Component({
   selector: 'app-searchbar',
-  imports: [NgIcon, FormsModule, IconButton],
+  imports: [NgIcon, FormsModule],
   viewProviders: [provideIcons({ heroMagnifyingGlass, heroXMark })],
   templateUrl: './searchbar.html'
 })
