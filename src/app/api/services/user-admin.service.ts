da92dcfbafa056be4bc1da65770598e3c4e2bac7
ElAguilaPrograma <je.munozm@outlook.com>
2026-03-12 00:40:23 -0600

    feat: add pagination to user-admin

---
+++ src/app/api/services/user-admin.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, inject } from "@angular/core";
+import { environment } from "../../env/enviroment";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { IUser } from "../interfaces/IUser";
+import { IPagedResult } from "../interfaces/IPagedResult";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserAdminService {
+    private readonly apiUrl = `${environment.apiUrl}/Authentication`;
+    private http = inject(HttpClient);
+
+    getUsers(page: number = 1, pageSize: number = 5): Observable<IPagedResult<IUser>> {
+        const params = new HttpParams()
+            .set('page', page.toString())
+            .set('pageSize', pageSize.toString());
+        return this.http.get<IPagedResult<IUser>>(`${this.apiUrl}/getallusers`, { params, withCredentials: true });
+    }
+
+    activateAndDeactivateUsers(userId: string) {
+        return this.http.post(`${this.apiUrl}/deactivateauser/${userId}`, { withCredentials: true });
+    }
+}
--- src/app/features/admin/pages/user-admin/user-admin.ts
+++ src/app/features/admin/pages/user-admin/user-admin.ts
@@ -1,23 +1,119 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from "../../../../shared/components/button/button.component";
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { heroUserPlus, heroMagnifyingGlass, heroPencilSquare, heroTrash } from '@ng-icons/heroicons/outline';
-
+import { heroUserPlus, heroMagnifyingGlass, heroPencilSquare, heroTrash, heroPower } from '@ng-icons/heroicons/outline';
 import { Router } from '@angular/router';
+import { UserAdminService } from '../../../../api/services/user-admin.service';
+import { IUser } from '../../../../api/interfaces/IUser';
+import { ConfirmDialog } from "../../../../shared/components/confirm-dialog/confirm-dialog";
 
 @Component({
   selector: 'app-user-admin',
-  imports: [CommonModule, ButtonComponent, NgIcon],
-  viewProviders: [provideIcons({ heroUserPlus, heroMagnifyingGlass, heroPencilSquare, heroTrash })],
+  imports: [CommonModule, ButtonComponent, NgIcon, ConfirmDialog],
+  viewProviders: [provideIcons({ heroUserPlus, heroMagnifyingGlass, heroPencilSquare, heroTrash, heroPower })],
   templateUrl: './user-admin.html',
   styleUrl: './user-admin.css',
 })
-export class UserAdmin {
+export class UserAdmin implements OnInit {
   private router = inject(Router);
+  private userAdminService = inject(UserAdminService);
+  private cdr = inject(ChangeDetectorRef);
+
+  users: IUser[] = [];
+  isLoading: boolean = false;
+  errorMessage: string = '';
+  disabledBotton: boolean = false;
+
+  currentPage: number = 1;
+  pageSize: number = 5;
+  totalCount: number = 0;
+  totalPages: number = 0;
+
+  pageNumbers: number[] = [];
+
+  isOpen = signal(false);
+  showConfirmDialog = signal(false);
+  selectedUserId: string = '';
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
 
   navigateToCreateUser() {
     this.router.navigate(['/user-form']);
   }
-}
 
+  setPageNumbers(): void {
+    this.pageNumbers = Array.from({ length: this.totalPages }, (_, i) => i + 1);
+  }
+
+  goToNextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.loadUsers();
+    }
+  }
+
+  goToPreviousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadUsers();
+    }
+  }
+
+  goToPage(page: number): void {
+    if (page >= 1 && page <= this.totalPages && page !== this.currentPage) {
+      this.currentPage = page;
+      this.loadUsers();
+    }
+  }
+
+  loadUsers(): void {
+    this.isLoading = true;
+    this.disabledBotton = true;
+    this.userAdminService.getUsers(this.currentPage, this.pageSize).subscribe({
+      next: (res) => {
+        this.users = res.data;
+        this.totalCount = res.totalCount;
+        this.totalPages = res.totalPages;
+        this.currentPage = res.page;
+        this.setPageNumbers();
+        this.isLoading = false;
+        this.disabledBotton = false;
+        
+        this.cdr.detectChanges();
+        
+        console.log('[UserAdmin] Users loaded successfully:', this.users);
+      },
+      error: (err) => {
+        this.errorMessage = 'Error loading users: ' + err;
+        console.error('[UserAdmin] Error al cargar usuarios:', err);
+        this.isLoading = false;
+        this.disabledBotton = false;
+      }
+    });
+  }
+
+  openConfirmDialog(): void {
+    this.showConfirmDialog.set(true);
+  }
+
+  closeConfirmDialog(): void {
+    this.showConfirmDialog.set(false);
+    this.selectedUserId = '';
+  }
+
+  activateAndDeactivateUsers(userId: string) {
+    this.userAdminService.activateAndDeactivateUsers(userId).subscribe({
+      next: () => {
+        this.loadUsers();
+        this.closeConfirmDialog();
+      },
+      error: (err) => {
+        this.errorMessage = 'Error activating and deactivating user: ' + err;
+        console.error('[UserAdmin] Error activating and deactivating user:', err);
+      }
+    });
+  }
+}
